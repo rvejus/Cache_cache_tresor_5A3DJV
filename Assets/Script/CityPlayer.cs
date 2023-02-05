423fae8c044cc9b3c3144aed3f62106201dc4f6e
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2023-02-05 16:57:05 +0100

    player script

---
+++ Assets/Script/CityPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityPlayer : MonoBehaviour
+{
+    public int playerID;
+    public bool isReady;
+
+    public void callAim()
+    {
+        
+    }
+}
